267aa7843a3ec308cea46e38a49af3a4b60fa66f
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2025-01-27 23:28:09 +0800

    Created packet sender helper

---
+++ server/internal/network/send.go
@@ -0,0 +1,19 @@
+package network
+
+import (
+	"net"
+	"server/internal/protocol"
+)
+
+// SendPacket is responsible for sending the packet to the given address.
+func SendPacket(c *net.UDPConn, a *net.UDPAddr, p *protocol.Packet) error {
+
+	data, err := p.ToBytes()
+	if err != nil {
+		return err
+	}
+	if _, errSend := c.WriteToUDP(data, a); errSend != nil {
+		return errSend
+	}
+	return nil
+}
