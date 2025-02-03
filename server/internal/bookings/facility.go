efc4a3cf23121a83d29d4d29dd4a95e271a6068c
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2025-02-03 19:31:38 +0800

    Added facility and its functionality

---
+++ server/internal/bookings/facility.go
@@ -0,0 +1,152 @@
+package bookings
+
+import (
+	"errors"
+	"log/slog"
+	"slices"
+	"sync"
+	"time"
+)
+
+type FacilityName string
+
+type Facility struct {
+	sync.Mutex
+	Name     FacilityName
+	Bookings []*Booking
+}
+
+func NewFacility(name FacilityName) *Facility {
+	return &Facility{
+		Name:     name,
+		Bookings: []*Booking{},
+	}
+}
+
+// clean clears up outdated bookings from the system
+func (f *Facility) clean() {
+
+	currentTime := time.Now()
+
+	// Remove bookings that have already finished
+	f.Bookings = slices.DeleteFunc(f.Bookings, func(b *Booking) bool {
+		return b.End.Before(currentTime)
+	})
+}
+
+// insertBooking attempts to insert a booking in sorted order
+func (f *Facility) insertBooking(newBooking *Booking) bool {
+	// Find the correct insertion index
+	index, found := slices.BinarySearchFunc(f.Bookings, newBooking, func(a, b *Booking) int {
+		if a.Start.Before(b.Start) {
+			return -1
+		}
+		if a.Start.After(b.Start) {
+			return 1
+		}
+		return 0
+	})
+
+	// Ensure no overlap
+	if found || (index > 0 && f.Bookings[index-1].Overlaps(newBooking)) ||
+		(index < len(f.Bookings) && f.Bookings[index].Overlaps(newBooking)) {
+		return false // Conflict detected
+	}
+
+	// Insert at the correct position
+	f.Bookings = append(f.Bookings[:index], append([]*Booking{newBooking}, f.Bookings[index:]...)...)
+	return true
+}
+
+// QueryAvailability searches for availability of the facility for the next number of nDays (including today)
+// Returns:
+// - []byte, where each bit represents the availability of the facility corresponding to the hour.
+func (f *Facility) QueryAvailability(nDays int) []byte {
+	f.Lock()
+	defer f.Unlock()
+	f.clean()
+
+	schedule := make([]byte, nDays*3)
+
+	currentTime := time.Now()
+	firstDate := time.Date(currentTime.Year(), currentTime.Month(), currentTime.Day(), 0, 0, 0, 0, time.Local)
+
+	for _, b := range f.Bookings {
+		if b.End.Before(firstDate) {
+			continue
+		}
+
+		firstHour := int(b.Start.Sub(firstDate).Hours())
+		lastHour := int(b.End.Sub(firstDate).Hours())
+
+		if firstHour < 0 {
+			firstHour = 0 // floor
+		}
+		if lastHour >= nDays*24 {
+			lastHour = nDays*24 - 1 // Booking ends after the queried period
+		}
+
+		// Set bits for each hour in the schedule
+		for hour := firstHour; hour < lastHour; hour++ {
+			byteIndex := hour / 8                // of the 3 bytes per, which byte is responsible for this timing
+			bitIndex := 7 - (hour % 8)           // of the 8 bits, which bit is responsible for the hour
+			schedule[byteIndex] |= 1 << bitIndex // set corresponding hour to be 1
+		}
+
+	}
+
+	return schedule
+}
+
+func (f *Facility) Book(b Booking) error {
+	f.Lock()
+	defer f.Unlock()
+	f.clean()
+
+	if !f.insertBooking(&b) {
+		slog.Error("Unable to insert booking due to clashes")
+		return errors.New("unable to insert booking due to clashes")
+	}
+
+	return nil
+}
+
+func (f *Facility) UpdateBooking(id uint16, deltaHours int) error {
+	f.Lock()
+	defer f.Unlock()
+	f.clean()
+
+	// Get index of ID
+	index := -1
+	var booking Booking
+	for i, b := range f.Bookings {
+		if b.Id == id {
+			index = i
+			booking = *b
+			break
+		}
+	}
+	if index == -1 {
+		slog.Error("Unable to find booking to be updated")
+		return errors.New("booking with specified ID not found")
+	}
+
+	// Remove existing booking
+	f.Bookings = append(f.Bookings[:index], f.Bookings[index+1:]...)
+
+	// Updating booking timing
+	newBooking := booking // create a copy of the existing booking
+	newBooking.Start = newBooking.Start.Add(time.Duration(deltaHours) * time.Hour)
+	newBooking.End = newBooking.End.Add(time.Duration(deltaHours) * time.Hour)
+
+	// Attempt to insert the updated booking
+	if ok := f.insertBooking(&newBooking); !ok {
+		// Fall back to original booking
+		_ = f.insertBooking(&booking)
+		slog.Error("Unable to update booking")
+		return errors.New("unable to modify booking due to clashes")
+	}
+
+	slog.Info("Booking has been updated", "OriginalBooking", booking, "NewBooking", newBooking)
+	return nil
+}
---
+++ server/internal/bookings/facility_test.go
@@ -0,0 +1,152 @@
+package bookings
+
+import (
+	"bytes"
+	"testing"
+	"time"
+)
+
+func TestFacility_clean(t *testing.T) {
+
+	timeNow := time.Now()
+
+	old := &Booking{
+		Id:    1,
+		Start: timeNow.Add(time.Duration(-5) * time.Hour),
+		End:   timeNow.Add(time.Duration(-4) * time.Hour),
+	}
+
+	current := &Booking{
+		Id:    1,
+		Start: timeNow.Add(time.Duration(-1) * time.Hour),
+		End:   timeNow.Add(time.Duration(2) * time.Hour),
+	}
+
+	future := &Booking{
+		Id:    1,
+		Start: timeNow.Add(time.Duration(5) * time.Hour),
+		End:   timeNow.Add(time.Duration(7) * time.Hour),
+	}
+
+	f := NewFacility(FacilityName("Testing"))
+
+	f.Bookings = []*Booking{old}
+	f.clean()
+	if len(f.Bookings) != 0 {
+		t.Error("clean() failed to clean up old booking")
+	}
+
+	f.Bookings = []*Booking{current, future}
+	f.clean()
+	if len(f.Bookings) != 2 {
+		t.Error("clean() cleaned up a bit too much")
+	}
+
+}
+
+func TestFacility_QueryAvailability(t *testing.T) {
+	currentTime := time.Now()
+	tmr := time.Date(currentTime.Year(), currentTime.Month(), currentTime.Day()+1, 0, 0, 0, 0, time.Local)
+
+	b1 := &Booking{
+		Id:    1,
+		Start: tmr.Add(time.Duration(0) * time.Hour),
+		End:   tmr.Add(time.Duration(3) * time.Hour),
+	}
+
+	f := NewFacility(FacilityName("Testing"))
+
+	_ = f.Book(*b1)
+
+	expectedByteArr := []byte{0x00, 0x00, 0x00, 0xE0, 0x00, 0x00}
+
+	if !bytes.Equal(f.QueryAvailability(2), expectedByteArr) {
+		t.Logf("E: % X", expectedByteArr)
+		t.Logf("R: % X", f.QueryAvailability(2))
+		t.Error("Availability does not match expected")
+	}
+
+}
+
+func TestFacility_Book(t *testing.T) {
+
+	currentTime := time.Now()
+
+	b1 := &Booking{
+		Id:    1,
+		Start: currentTime.Add(time.Duration(1) * time.Hour),
+		End:   currentTime.Add(time.Duration(3) * time.Hour),
+	}
+	b2 := &Booking{
+		Id:    2,
+		Start: currentTime.Add(time.Duration(3) * time.Hour),
+		End:   currentTime.Add(time.Duration(4) * time.Hour),
+	}
+	b3 := &Booking{
+		Id:    3,
+		Start: currentTime.Add(time.Duration(1) * time.Hour),
+		End:   currentTime.Add(time.Duration(2) * time.Hour),
+	}
+
+	f := NewFacility(FacilityName("Testing"))
+
+	if err := f.Book(*b1); err != nil {
+		t.Error("Unexpected booking error")
+	}
+	if len(f.Bookings) != 1 {
+		t.Error("Booking was not inserted properly")
+	}
+
+	if err := f.Book(*b2); err != nil {
+		t.Error("Unexpected booking error")
+	}
+	if len(f.Bookings) != 2 {
+		t.Error("Booking was not inserted properly")
+	}
+
+	if err := f.Book(*b3); err == nil {
+		t.Error("Expected booking clash error, but none were raised")
+	}
+	if len(f.Bookings) != 2 {
+		t.Error("Booking was not inserted properly")
+	}
+
+}
+
+func TestFacility_UpdateBooking(t *testing.T) {
+
+	currentTime := time.Now()
+
+	b1 := &Booking{
+		Id:    1,
+		Start: currentTime.Add(time.Duration(1) * time.Hour),
+		End:   currentTime.Add(time.Duration(3) * time.Hour),
+	}
+	b2 := &Booking{
+		Id:    2,
+		Start: currentTime.Add(time.Duration(3) * time.Hour),
+		End:   currentTime.Add(time.Duration(4) * time.Hour),
+	}
+
+	f := NewFacility(FacilityName("Testing"))
+
+	_ = f.Book(*b1)
+	_ = f.Book(*b2)
+
+	// Success case
+	if err := f.UpdateBooking(b2.Id, 1); err != nil {
+		t.Error("Error while updating booking")
+	}
+	if len(f.Bookings) != 2 {
+		t.Error("Unexpected booking deletion upon update")
+	}
+	if f.Bookings[1].Start != b2.Start.Add(time.Duration(1)*time.Hour) || f.Bookings[1].End != b2.End.Add(time.Duration(1)*time.Hour) {
+		t.Error("Booking timing was not updated properly")
+	}
+
+	// Error case
+	if err := f.UpdateBooking(b2.Id, -2); err == nil {
+		t.Error("Expected an error to be raised upon error update")
+	}
+
+}
