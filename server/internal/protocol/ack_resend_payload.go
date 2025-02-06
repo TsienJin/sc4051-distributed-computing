b9ef5ca24e1cc305d1c23fd748b2b8fcb3a0091f
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2025-02-06 16:15:12 +0800

    Added helper methods to ack/resend payload

--- server/internal/protocol/ack_resend_payload.go
+++ server/internal/protocol/ack_resend_payload.go
@@ -1,6 +1,8 @@
 package protocol
 
-import "server/internal/protocol/proto_defs"
+import (
+	"server/internal/protocol/proto_defs"
+)
 
 // AckResendPayload is the payload layout for Acknowledgements and Resend packets
 type AckResendPayload struct {
@@ -10,4 +12,17 @@ type AckResendPayload struct {
 
 func (a *AckResendPayload) MarshalBinary() ([]byte, error) {
 	return append(a.Id[:], a.PacketNumber), nil
+}
+
+func (a *AckResendPayload) UnmarshalBinary(data []byte) error {
+	a.Id = proto_defs.MessageId(data[0:4])
+	a.PacketNumber = data[4]
+	return nil
+}
+
+func (a *AckResendPayload) ToPacketIdent() *PacketIdent {
+	return &PacketIdent{
+		MessageId:    a.Id,
+		PacketNumber: a.PacketNumber,
+	}
 }
