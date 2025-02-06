5d7fde6e79b05f5eb1d7078835cc7d846ed9439c
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2025-02-06 16:19:55 +0800

    Added handler for incoming messages

--- server/internal/handle/handle_incoming_message.go
+++ server/internal/handle/handle_incoming_message.go
@@ -1,10 +1,22 @@
 package handle
 
 import (
+	"log/slog"
 	"net"
+	"server/internal/handle/handle_requests"
 	"server/internal/protocol"
+	"server/internal/protocol/proto_defs"
 )
 
 func IncomingMessage(c *net.UDPConn, a *net.UDPAddr, m *protocol.Message) {
 
+	slog.Info("Handling message", "MessageType", m.Header.MessageType, "MessageId", m.Header.MessageId)
+
+	switch m.Header.MessageType {
+	case proto_defs.MessageTypeRequest:
+		handle_requests.Sort(c, a, m)
+		break
+	default:
+		slog.Error("Message type not supported yet", "MessageType", m.Header.MessageType)
+	}
 }
