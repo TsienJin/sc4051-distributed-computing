88c3208b3c895c660824bae89074722beb6b8fa7
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2025-02-06 17:10:12 +0800

    Fixed memory leak in TCP log watchers

--- server/internal/logging/handle_client_messages.go
+++ server/internal/logging/handle_client_messages.go
@@ -2,6 +2,8 @@ package logging
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"log/slog"
 	"net"
 )
@@ -13,6 +15,13 @@ func HandleClientMessages(c *net.TCPConn) {
 	for {
 		n, err := c.Read(buf)
 		if err != nil {
+			// Check if the error indicates that the connection is closed.
+			if err == io.EOF {
+				log.Printf("[CLIENT] Connection from %s closed by the client", c.RemoteAddr().String())
+				return // Exit the loop (and function) if the connection is closed.
+			}
+			// Handle other potential errors.
+			log.Printf("[CLIENT] Error reading from %s: %v", c.RemoteAddr().String(), err)
 			continue
 		}
 		slog.Info(fmt.Sprintf("[CLIENT:%s] %s", c.RemoteAddr().String(), string(buf[:n])))
