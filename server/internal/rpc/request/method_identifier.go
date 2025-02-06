1bd52340d8e7d9c629515ec5b2f67d51d8b7a7f8
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2025-02-06 16:14:22 +0800

    Prefixed method ident names

--- server/internal/rpc/request/method_identifier.go
+++ server/internal/rpc/request/method_identifier.go
@@ -3,12 +3,12 @@ package request
 type MethodIdentifier uint8
 
 const (
-	FacilityCreate  MethodIdentifier = 0x01
-	FacilityQuery   MethodIdentifier = 0x02
-	FacilityMonitor MethodIdentifier = 0x03
-	FacilityDelete  MethodIdentifier = 0x04
+	MethodIdentifierFacilityCreate  MethodIdentifier = 0x01
+	MethodIdentifierFacilityQuery   MethodIdentifier = 0x02
+	MethodIdentifierFacilityMonitor MethodIdentifier = 0x03
+	MethodIdentifierFacilityDelete  MethodIdentifier = 0x04
 
-	BookingMake   MethodIdentifier = 0x11
-	BookingUpdate MethodIdentifier = 0x12
-	BookingDelete MethodIdentifier = 0x13
+	MethodIdentifierBookingMake   MethodIdentifier = 0x11
+	MethodIdentifierBookingUpdate MethodIdentifier = 0x12
+	MethodIdentifierBookingDelete MethodIdentifier = 0x13
 )
