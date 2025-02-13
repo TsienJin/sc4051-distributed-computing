0c1c00b6d4afd652e656bdbd505345be9fd9bd25
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2025-02-14 02:36:13 +0800

    Added un/marshalling

--- server/internal/rpc/request/facility_create_payload.go
+++ server/internal/rpc/request/facility_create_payload.go
@@ -6,6 +6,10 @@ type FacilityCreatePayload struct {
 	Name bookings.FacilityName
 }
 
+func (f *FacilityCreatePayload) MarshalBinary() ([]byte, error) {
+	return []byte(f.Name), nil
+}
+
 func (f *FacilityCreatePayload) UnmarshalBinary(data []byte) error {
 	f.Name = bookings.FacilityName(data)
 	return nil
--- server/internal/rpc/request/request.go
+++ server/internal/rpc/request/request.go
@@ -1,5 +1,10 @@
 package request
 
+import (
+	"bytes"
+	"encoding/binary"
+)
+
 type Request struct {
 	MethodIdentifier MethodIdentifier
 	Payload          []byte
@@ -9,4 +14,19 @@ func (r *Request) UnmarshalBinary(data []byte) error {
 	r.MethodIdentifier = MethodIdentifier(data[0])
 	r.Payload = data[1:]
 	return nil
+}
+
+func (r *Request) MarshalBinary() ([]byte, error) {
+
+	buf := &bytes.Buffer{}
+
+	if err := binary.Write(buf, binary.BigEndian, r.MethodIdentifier); err != nil {
+		return nil, err
+	}
+
+	if err := binary.Write(buf, binary.BigEndian, r.Payload); err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
 }
