a56f962a0b5183dbfd5cacf7e6c3702377404b28
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2025-02-15 02:14:35 +0800

    Created Create Facility Req impl

---
+++ server/internal/rpc/request/constructor_facility_create.go
@@ -0,0 +1,41 @@
+package request
+
+import (
+	"server/internal/bookings"
+	"server/internal/interfaces"
+	"server/internal/protocol"
+	"server/internal/protocol/proto_defs"
+)
+
+func NewFacilityCreatePacket(name string) interfaces.RpcRequestConstructor {
+
+	return func() ([]*protocol.Packet, error) {
+		payload := &FacilityCreatePayload{
+			Name: bookings.FacilityName(name),
+		}
+
+		payloadByte, err := payload.MarshalBinary()
+		if err != nil {
+			return nil, err
+		}
+
+		r := Request{
+			MethodIdentifier: MethodIdentifierFacilityCreate,
+			Payload:          payloadByte,
+		}
+
+		headerDistilled := &protocol.PacketHeaderDistilled{
+			Version:     proto_defs.ProtocolV1,
+			MessageId:   proto_defs.NewMessageId(),
+			MessageType: proto_defs.MessageTypeRequest,
+			RequireAck:  true,
+		}
+
+		message, err := protocol.NewMessage(headerDistilled, &r)
+		if err != nil {
+			return nil, err
+		}
+
+		return message.ToPackets()
+	}
+}
