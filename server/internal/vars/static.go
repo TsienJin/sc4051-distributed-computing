bce0874ecd71ad4898be2a39f521a436179ea899
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2025-03-27 14:23:40 +0800

    Added new env var to disable duplicate filtering

--- server/internal/vars/static.go
+++ server/internal/vars/static.go
@@ -11,6 +11,7 @@ type StaticEnvStruct struct {
 	ServerPort    int `env:"SERVER_PORT" envDefault:"8765"`     // Port exposed for the actual booking application
 	ServerLogPort int `env:"SERVER_LOG_PORT" envDefault:"7777"` // Port exposed for logs to be viewed remotely
 
+	EnableDuplicateFiltering  bool    `env:"ENABLE_DUPLICATE_FILTERING" envDefault:"true"`
 	PacketDropRate            float32 `env:"PACKET_DROP_RATE" envDefault:"0.30"`         // Rate of which packets are dropped (in and out)
 	PacketReceiveTimeout      int     `env:"PACKET_TIMEOUT_RECEIVE" envDefault:"50"`     // Timeout for packets received and unacked in milliseconds
 	PacketTTL                 int     `env:"PACKET_TTL" envDefault:"5000"`               // Maximum time to keep packets in history
@@ -43,6 +44,12 @@ func GetStaticEnv() *StaticEnvStruct {
 
 func GetStaticEnvCopy() StaticEnvStruct {
 	return *GetStaticEnv()
+}
+
+func SetEnableDuplicateFiltering(val bool) error {
+	GetStaticEnv().EnableDuplicateFiltering = val
+	slog.Info("[ENV] EnableDuplicateFiltering has been updated", "val", val)
+	return nil
 }
 
 func SetPacketDropRate(val float32) error {
