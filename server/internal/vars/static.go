eb081ab1eb8e779a131bfbea5e2942a3ddea68af
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2025-03-28 23:05:26 +0800

    Updated env vars

--- server/internal/vars/static.go
+++ server/internal/vars/static.go
@@ -12,12 +12,12 @@ type StaticEnvStruct struct {
 	ServerLogPort int `env:"SERVER_LOG_PORT" envDefault:"7777"` // Port exposed for logs to be viewed remotely
 
 	EnableDuplicateFiltering  bool    `env:"ENABLE_DUPLICATE_FILTERING" envDefault:"true"`
-	PacketDropRate            float32 `env:"PACKET_DROP_RATE" envDefault:"0.30"`         // Rate of which packets are dropped (in and out)
-	PacketReceiveTimeout      int     `env:"PACKET_TIMEOUT_RECEIVE" envDefault:"50"`     // Timeout for packets received and unacked in milliseconds
+	PacketDropRate            float32 `env:"PACKET_DROP_RATE" envDefault:"0.70"`         // Rate of which packets are dropped (in and out)
+	PacketReceiveTimeout      int     `env:"PACKET_TIMEOUT_RECEIVE" envDefault:"20"`     // Timeout for packets received and unacked in milliseconds
 	PacketTTL                 int     `env:"PACKET_TTL" envDefault:"5000"`               // Maximum time to keep packets in history
 	MessageAssemblerIntervals int     `env:"MESSAGE_ASSEMBLER_INTERVAL" envDefault:"50"` // Time between runs to request missing packets
 	ResponseTTL               int     `env:"RESPONSE_TTL" envDefault:"10000"`            // Maximum time to keep messages in history
-	ResponseIntervals         int     `env:"RESPONSE_INTERVAL" envDefault:"50"`          // Time between runs to check for expired responses
+	ResponseIntervals         int     `env:"RESPONSE_INTERVAL" envDefault:"500"`         // Time between runs to check for expired responses
 
 	MatterMostWebhook string `env:"MATTERMOST_WEBHOOK" envDefault:""`
 }
